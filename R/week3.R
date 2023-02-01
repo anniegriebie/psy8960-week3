1ce479ff8a81f3f8c8da15808cfc34b500c0162f
anniegriebie <GRIEB046@umn.edu>
2023-02-01 13:58:09 -0600

    Recast time variables into POSIX format

--- R/week3.R
+++ R/week3.R
@@ -2,4 +2,6 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
-raw_df<- read.csv("../data/week3.csv")
+raw_df<- read.csv("../data/week3.csv", header = TRUE)
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart, format = "%Y-%m-%d %H:%M:%S")
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, format = "%Y-%m-%d %H:%M:%S")
